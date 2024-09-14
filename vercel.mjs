b1b8990ddaaaaaf9cecc9aea389cbd6e7380f4ed
Diego Condo <diegocondo1007@gmail.com>
2024-09-14 13:11:22 -0500

    FIX: ajustes de vercel.mjs

--- vercel.mjs
+++ vercel.mjs
@@ -1,33 +1,34 @@
-import fs from "fs";
-import path from "path";
-import { fileURLToPath } from "url";
+// import fs from "fs";
+// import path from "path";
+// import { fileURLToPath } from "url";
 
-// Obtener la variable de entorno
-const apiBaseUrl = process.env.NEXT_PUBLIC_API_BASE_URL_SERVER;
+// // Obtener la variable de entorno
+// const apiBaseUrl = process.env.NEXT_PUBLIC_API_BASE_URL_SERVER;
 
-if (!apiBaseUrl) {
-  console.error("Error: La variable NEXT_PUBLIC_API_BASE_URL_SERVER no está definida.");
-  process.exit(1);
-}
+// if (!apiBaseUrl) {
+//   console.error("Error: La variable NEXT_PUBLIC_API_BASE_URL_SERVER no está definida.");
+//   process.exit(1);
+// }
 
-// Configuración del archivo vercel.json
-const vercelConfig = {
-  rewrites: [
-    {
-      source: "/v1/:path*", // Cambia el patrón para capturar cualquier cosa después de /v1/
-      destination: `${apiBaseUrl}/api/v1/:path*`, // Añade /api/v1/ en el destino
-    },
-  ],
-};
+// // Configuración del archivo vercel.json
+// const vercelConfig = {
+//   rewrites: [
+//     {
+//       source: "/v1/:path*", // Cambia el patrón para capturar cualquier cosa después de /v1/
+//       destination: `${apiBaseUrl}/api/v1/:path*`, // Añade /api/v1/ en el destino
+//     },
+//   ],
+// };
 
-// Obtener el directorio actual utilizando `import.meta.url`
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+// // Obtener el directorio actual utilizando `import.meta.url`
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
 
-// Ruta donde se guardará el archivo vercel.json
-const vercelConfigPath = path.join(__dirname, "vercel.json");
+// // Ruta donde se guardará el archivo vercel.json
+// const vercelConfigPath = path.join(__dirname, "vercel.json");
 
-// Escribir el archivo vercel.json
-fs.writeFileSync(vercelConfigPath, JSON.stringify(vercelConfig, null, 2));
+// // Escribir el archivo vercel.json
+// fs.writeFileSync(vercelConfigPath, JSON.stringify(vercelConfig, null, 2));
 
-console.log("Archivo vercel.json generado con éxito:", vercelConfig);
+// console.log("Archivo vercel.json generado con éxito:", vercelConfig);
+console.log("ENV: ", process.env);
