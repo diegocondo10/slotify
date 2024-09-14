ae60e549933dbba1b3ceb486beed59197ab8742b
Diego Condo <diegocondo1007@gmail.com>
2024-09-14 11:35:04 -0500

    FIX: ajustes para ambientes de uat

--- next.config.mjs
+++ next.config.mjs
@@ -1,23 +1,35 @@
 // Importa el plugin `next-pwa`
 import withPWA from "next-pwa";
 
+const isDev = process.env.NODE_ENV === "development";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Configuración específica de Next.js
   typescript: {
-    ignoreBuildErrors: true, // Configuración de TypeScript
+    ignoreBuildErrors: true,
   },
   experimental: {
-    serverActions: true, // Características experimentales
+    serverActions: true,
   },
-  reactStrictMode: false, // Desactiva el modo estricto de React
-  compress: true, // Habilita la compresión
+  reactStrictMode: false,
+  compress: true,
+  async redirects() {
+    if (isDev) {
+      return [];
+    }
+
+    return [
+      {
+        source: "/v1/:path*",
+        destination: `${process.env.NEXT_PUBLIC_API_BASE_URL}:path*`,
+      },
+    ];
+  },
 };
 
-// Combina la configuración de PWA con la configuración de Next.js
 export default withPWA({
   dest: "public",
-  disable: false,
+  disable: isDev,
   register: true,
   skipWaiting: true,
   disableDevLogs: true,
